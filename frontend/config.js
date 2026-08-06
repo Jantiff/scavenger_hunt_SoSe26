c90aa65416fcbdeb6aa9a94d664d2430cd31ad22
Fabian Wottke <fabian.wottke@t-online.de>
2026-08-06 22:36:38 +0200

    Added Myself to INfo

--- frontend/config.js
+++ frontend/config.js
@@ -2,7 +2,7 @@ const config = {
   linkOfImage:
     "/assets/hochschule-aalen-logo.svg",
   infoText:
-    "Entwickelt von Hikmet Gözaydin und Niklas Fichtner an der Hochschule Aalen unter der Leitung von Dr. Marc Hermann",
+    "Entwickelt von Hikmet Gözaydin und Niklas Fichtner an der Hochschule Aalen unter der Leitung von Dr. Marc Hermann. Zusätzlich hat Fabian Wottke das Farbkonzept und verschiedene Designverbesserungen umgesetzt sowie den QR-Code-Scanner in die Anwendung integriert.",
 };
 
 export default config;
